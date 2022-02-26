adaca3afd71ddc73c1c1f89a1060215aad3db407
acalvom <andreac.3108@gmail.com>
2022-02-26 18:10:55 +0100

    Add route for details phone end-point #9

--- app/phoneRoutes.js
+++ app/phoneRoutes.js
@@ -10,5 +10,7 @@ router.get('/phone/:id', phoneController.getPhoneById);
 router.delete('/phone/:id', phoneController.deletePhoneById);
 router.post('/add', upload.single('phoneImage'), phoneController.postPhone);
 router.get('/uploads/:filename', phoneController.getImageByFilename);
+router.get('/phone/uploads/:filename', phoneController.getImageByFilename);
+
 
 module.exports = router;
