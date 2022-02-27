a94f96c669e2b96dcd2034d38a5f6beb694d05fd
acalvom <andreac.3108@gmail.com>
2022-02-27 16:04:35 +0100

    Load React app from Back-End #11

--- index.js
+++ index.js
@@ -10,15 +10,21 @@ app.disable("x-powered-by");
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-
 app.options('*', cors());
 app.use(cors());
+
+app.use(express.static('build'));
 
 app.get('/', (req, res) => {
     res.send('index.js')
 })
 
 app.use(phonesRoutes);
+
+// For React Router
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'))
+})
 
 app.listen(port, function () {
     if (port === 8000)
